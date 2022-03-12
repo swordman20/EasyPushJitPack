729a740b8e5f651314e979c4ac92deba9d89ba0d
Hades <xiaweifeng@live.cn>
2022-03-12 20:16:45 +0800

    Test  Library

--- app/src/main/java/com/example/easypushjitpack/MainActivity.kt
+++ app/src/main/java/com/example/easypushjitpack/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.easypushjitpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mylibrary.MyLog
+import com.example.mylibrary.Student
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val student = Student("张三", 18, "男")
+        MyLog.d(student.toString())
+        MyLog.add(5)
     }
 }
